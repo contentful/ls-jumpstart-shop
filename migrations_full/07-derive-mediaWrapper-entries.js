3d9e131bcd8df29527f1819279a54b2815632068
chigoriddim <106765797+chigoriddim@users.noreply.github.com>
2023-05-09 15:12:55 +0200

    update migrations_full/07-derive-mediaWrapper-entries.js

--- migrations_full/07-derive-mediaWrapper-entries.js
+++ migrations_full/07-derive-mediaWrapper-entries.js
@@ -7,48 +7,48 @@ transformEntriesToType from product to mediaWrapper
 
 const _ = require("lodash");
 
-module.exports = function(migration) {
-    migration.transformEntriesToType({
-        sourceContentType: "product",
-        targetContentType: "mediaWrapper",
-        from: ["image", "slug"],
-        shouldPublish: true,
-        updateReferences: false,
-        removeOldEntries: false,
-        identityKey: function(fields) {
-            try {
-                const slug = _.get(fields, "slug['en-US']");
-                if (slug) {
-                    return slug;
-                }
-            } catch (error) {}
-        },
-        transformEntryForLocale: function(fromFields, currentLocale) {
-            // fromFields is the product fields.
+module.exports = function (migration) {
+  migration.transformEntriesToType({
+    sourceContentType: "product",
+    targetContentType: "mediaWrapper",
+    from: ["image", "slug"],
+    shouldPublish: true,
+    updateReferences: false,
+    removeOldEntries: false,
+    identityKey: function (fields) {
+      try {
+        const slug = _.get(fields, "slug['en-US']");
+        if (slug) {
+          return slug;
+        }
+      } catch (error) {}
+    },
+    transformEntryForLocale: function (fromFields, currentLocale) {
+      // fromFields is the product fields.
 
-            try {
-                const oldImageId = _.get(fromFields, "image['en-US'].sys.id"); // id of existing image
+      try {
+        const oldImageId = _.get(fromFields, "image['en-US'].sys.id"); // id of existing image
 
-                const slug = _.get(fromFields, "slug['en-US']");
+        const slug = _.get(fromFields, "slug['en-US']");
 
-                if (oldImageId && slug) {
-                    const derivedAsset = {
-                        sys: { type: "Link", linkType: "Asset", id: oldImageId },
-                    }; //new asset with image id (oldImageId)
+        if (oldImageId && slug) {
+          const derivedAsset = {
+            sys: { type: "Link", linkType: "Asset", id: oldImageId },
+          }; //new asset with image id (oldImageId)
 
-                    let returnedObject = {};
-                    returnedObject.internalName = slug;
-                    returnedObject.title = slug;
-                    returnedObject.altText = "no altText"; // laziness :)
-                    returnedObject.image = derivedAsset;
+          let returnedObject = {};
+          returnedObject.internalName = slug;
+          returnedObject.title = slug;
+          returnedObject.altText = "no altText"; // laziness :)
+          returnedObject.image = derivedAsset;
 
-                    return returnedObject;
-                }
+          return returnedObject;
+        }
 
-                return false;
-            } catch (error) {
-                return false;
-            }
-        },
-    });
+        return false;
+      } catch (error) {
+        return false;
+      }
+    },
+  });
 };
